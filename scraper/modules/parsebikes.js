2ee393c5dc8b234d034bb1f1a6a619470eb31570
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2014-08-27 21:18:08 +0000

    I'd rather be setting priority explicitly while pushin

--- scraper/modules/parsebikes.js
+++ scraper/modules/parsebikes.js
@@ -1,15 +1,23 @@
 var https = require('https');
 var Firebase = require("firebase");
-var myFirebaseRef = new Firebase("https://bikeshistory.firebaseio.com/");
+var hash = require("string-hash");
+var myFirebaseRef = new Firebase("https://bikeshistory.firebaseio.com/states");
 var responseBuffer = "";
-var pushFirebaseRecord, parseBikes;
+var pushFirebaseRecord, parseBikes, d2h, conditionalPushRecord;
+
+d2h = function(d) {
+  return (+d).toString(16).toUpperCase();
+}
 
 pushFirebaseRecord = function(buffer) {
   // var re = /var mapDataLocations = [{.+}]/;
   var re = /var\s+mapDataLocations\s*=\s*(\[.+\])\s*\;/;
-  var capture, fullState, shortState = [], station;
+  var capture, fullState, shortState = [], station, nameHash;
+  var newItem;
   // buffer = 'var mapDataLocations = [{cghxzgchxgchxz}];';
   
+  // conditionalPushRecord(buffer);
+
   capture = re.exec(buffer)[1];
   // console.log(capture);
 
@@ -18,18 +26,33 @@ pushFirebaseRecord = function(buffer) {
   // console.log('fool: ', fullState);
   for (station in fullState) {
     // console.log('station: ', station, ', ', fullState[station]);
-    shortState.push({ AvailableBikesCount : fullState[station].AvailableBikesCount,
-                      FreeLocksCount: fullState[station].FreeLocksCount,
-		      LocalTitle: fullState[station].LocalTitle });
+    nameHash = hash(fullState[station].LocalTitle);
+    shortState.push({ A: fullState[station].AvailableBikesCount,
+                      F: fullState[station].FreeLocksCount,
+		      H: /*d2h(*/nameHash/*)*/ });
   }
 
-  myFirebaseRef.push({timestamp: Firebase.ServerValue.TIMESTAMP, state: shortState});
+  // myFirebaseRef.push({timestamp: Firebase.ServerValue.TIMESTAMP, state: shortState});
+  newItem = myFirebaseRef.push();
+  newItem.setWithPriority({timestamp: Firebase.ServerValue.TIMESTAMP, state: shortState}, Firebase.ServerValue.TIMESTAMP);
+}
+
+// push either short or long record, depending on the last modification time
+conditionalPushRecord = function(buffer) {
+// startAt(1409171793939).endAt(1409171793939).
+  myFirebaseRef.once('value',
+    function(dataSnapshot) { // success
+      console.log('Snapshot: ', dataSnapshot);
+    },
+    function(error) { // failure
+      console.log('myFirebaseRef.once("value") failed: ', error);
+    });
 }
 
 exports.parseBikes = parseBikes = function() {
 
   https.get("https://www.bikes-srm.pl/Mobile/LocationsMap.aspx", function(ress) {
-    console.log("Got response: " + ress.statusCode);
+    // console.log("Got response: " + ress.statusCode);
     // console.log(ress);
     responseBuffer = "";
 
@@ -40,7 +63,7 @@ exports.parseBikes = parseBikes = function() {
 
     // This never happens
     ress.on('end', function(){
-        console.log("End received!");
+        // console.log("End received!");
         pushFirebaseRecord(responseBuffer);
         responseBuffer = "";
     });
