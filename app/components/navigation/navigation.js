4788fc585500ff0389845f9f4e96e6ee5e90078a
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2014-11-23 23:15:53 +0100

    Added screen reader tags

--- app/components/navigation/navigation.js
+++ app/components/navigation/navigation.js
@@ -11,30 +11,37 @@ angular.module('myApp.navigation', [])
 				pattern: '/view1',
 				route: '#/view1',
 				description: 'View 1',
-				active: "" // set automatically in the event handler
+				active: "", // set automatically in the event handler
+				srClass: "", // ~
+				srText: "", // ~
 			},
 			{
 				pattern: '/view2',
 				route: '#/view2',
 				description: 'View 2',
-				active: ""
+				active: "",
+				srClass: "", // ~
+				srText: "" // ~
 			}
 		];
 
 		$scope.$on('$locationChangeStart', function(event, next, current) {
-			console.log('locationChangeStart: ', event, next, current);
+			// console.log('locationChangeStart: ', event, next, current);
 
 			var path = $location.path(), routeIndex = -1;
 			$scope.navigation.forEach(function(value, index, ar) {
-				console.log('iterating: ', value, index, ar);
-				if (path.indexOf(value.pattern) >= 0)
+				// console.log('iterating: ', value, index, ar);
+				if (path.indexOf(value.pattern) >= 0) {
 					value.active = "active";
-				else
+					value.srClass = "sr-only";
+					value.srText = "(current)";
+				}
+				else {
 					value.active = "";
+					value.srClass = "";
+					value.srText = "";
+				}
 			});
 
-		});
-		$scope.$on('$locationChangeSuccess', function(event, next, current) {
-			console.log('locationChangeSuccess: ', event, next, current);
 		});
 	}]);
