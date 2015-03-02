90d6f844e94d28929d5830f5e3d04c91b458246e
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-03-02 22:51:09 +0100

    Added callback to the payload

--- scraper/invokeidempotent/modules/payload.js
+++ scraper/invokeidempotent/modules/payload.js
@@ -2,8 +2,18 @@
 
 var parseBikesModule = require('../../modules/parsebikes.js');
 
-exports.handler = function() {
+exports.handler = function(callback) {
   var startingMoment = +new Date();
   console.log('payload: ', new Date(startingMoment).toString());
-  parseBikesModule.parseBikes();
+
+  var index = 0;
+  var theInterval = setInterval(function() {
+    var delta = +new Date() - startingMoment;
+    console.log('index: ', index++, new Date(delta).toString());
+    if (index > 5) {
+      clearInterval(theInterval);
+      callback(undefined, { status: true, msg: "Quit happily" });
+    }
+  }, (10 * 100));
+  // parseBikesModule.parseBikes();
 }
