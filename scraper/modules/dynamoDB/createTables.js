b57fab1d2a399411f69d88a62ab539a727dea9e5
Kris Dudkiewicz <doodeck@users.noreply.github.com>
2015-03-04 07:02:59 +0100

    Disabled table creation on module launch in lambda mode

--- scraper/modules/dynamoDB/createTables.js
+++ scraper/modules/dynamoDB/createTables.js
@@ -20,24 +20,25 @@ http://serverfault.com/questions/424549/using-dynamodb-for-logging-user-activity
 According to http://docs.aws.amazon.com/amazondynamodb/latest/developerguide/QueryAndScan.html
 Query results are always sorted by the range key. If the data type of the range key is Number, the results are returned in numeric order; otherwise, the results are returned in order of ASCII character code values. By default, the sort order is ascending. To reverse the order, set the ScanIndexForward parameter to false.
 */
-var params = {
-  AttributeDefinitions: [
-    { AttributeName: 'period', AttributeType: 'S' }, // e.g. '2014-10'
-    { AttributeName: 'timestamp', AttributeType: 'N' }
-  ],
-  KeySchema: [
-    { AttributeName: 'period', KeyType: 'HASH' },
-    { AttributeName: 'timestamp', KeyType: 'RANGE' }
-  ],
-  ProvisionedThroughput: { /* required */
-    ReadCapacityUnits: 12, /* required */
-    WriteCapacityUnits: 12 /* required */
-  },
-  TableName: config.AWS.dynamoDBtable
-};
+if (!config.AWS.usingLambda) {
+  var params = {
+    AttributeDefinitions: [
+      { AttributeName: 'period', AttributeType: 'S' }, // e.g. '2014-10'
+      { AttributeName: 'timestamp', AttributeType: 'N' }
+    ],
+    KeySchema: [
+      { AttributeName: 'period', KeyType: 'HASH' },
+      { AttributeName: 'timestamp', KeyType: 'RANGE' }
+    ],
+    ProvisionedThroughput: { /* required */
+      ReadCapacityUnits: 12, /* required */
+      WriteCapacityUnits: 12 /* required */
+    },
+    TableName: config.AWS.dynamoDBtable
+  };
 
-dynamodb.createTable(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
-
+  dynamodb.createTable(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+}
